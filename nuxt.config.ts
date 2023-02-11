7653eabce56dfb0828c8f2f140b1001d2d560d10
Defelo <elodef42@gmail.com>
2023-02-11 12:26:17 +0100

    Fix 404 responses for some pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
 	app: {
 		pageTransition: { name: 'page', mode: 'out-in' },
 		layoutTransition: { name: 'layout', mode: 'out-in' },
