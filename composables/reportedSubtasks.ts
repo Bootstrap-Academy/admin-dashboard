c5b5db1c9d463526e1bccc2ea3959a439bba0832
JohKei <kaebjohann@googlemail.com>
2023-11-25 15:50:48 +0100

    add types & get creator-id & userName

--- composables/reportedSubtasks.ts
+++ composables/reportedSubtasks.ts
@@ -1,128 +1,150 @@
-export const useReportedSubtasks = () => useState("reportedSubtasks", () => [])
-export const useReportedSubtask = () => useState("reportedSubtask", () => null)
-export const useOffsetReportedTasks = () => useState("offsetReportedTasks", () => 0)
-export const useLimitReportedTasks = () => useState("limitReportedTasks", () => 10)
-export const useReportReason = () => useCookie("reportReason")
-export const useReportSubtaskType = () => useCookie("reportSubtaskType")
-export const useCodingChallenge = () => useState("codingChallenge", () => null)
-export const useCodingChallengeSolution = () => useState("codingChallengeSolution", () => null)
-export const useMcq = () => useState("mcq", () => null)
-export const useNoMoreSubtasks = () => useState("noMoreSubtasks", () => false)
+import type { ReportBase } from "~/types/reportedTaskTypes";
 
+export const useReportedSubtasks = () =>
+	useState<ReportBase[]>("reportedSubtasks", () => []);
+export const useReportedSubtask = () => useState("reportedSubtask", () => null);
+export const useOffsetReportedTasks = () =>
+	useState("offsetReportedTasks", () => 0);
+export const useLimitReportedTasks = () =>
+	useState("limitReportedTasks", () => 10);
+export const useReportReason = () => useCookie("reportReason");
+export const useReportSubtaskType = () => useCookie("reportSubtaskType");
+export const useCodingChallenge = () => useState("codingChallenge", () => null);
+export const useCodingChallengeSolution = () =>
+	useState("codingChallengeSolution", () => null);
+export const useMcq = () => useState("mcq", () => null);
+export const useNoMoreSubtasks = () => useState("noMoreSubtasks", () => false);
 
-export async function getreportedSubtasksList(firstCall: any) {
-    try {
-        const limit = useLimitReportedTasks()
-        const offset = useOffsetReportedTasks()
-        const noMoreSubtasks = useNoMoreSubtasks()
-        // const response = await GET(`/challenges/subtask_reports`);
-        const response = await GET(`/challenges/subtask_reports?limit=${limit.value}&offset=${offset.value}`);
-        let arr = response ?? []
-        if (!arr.length) {
-            noMoreSubtasks.value = true
-            return openSnackbar("info", "Body.NoMoreReports")
-        }
-        if (arr.length < 10) {
-            noMoreSubtasks.value = true
-        }
-        const reportedSubtasks: any = useReportedSubtasks();
-        if (firstCall) {
-            reportedSubtasks.value = []
-        }
-        arr = [...reportedSubtasks.value, ...response ?? []]
-        console.log(reportedSubtasks.value, arr, 'getreportedSubtasksList');
-        
-        arr.forEach(async (subtask: any, index: any) => {
-            if (index >= reportedSubtasks.value.length) {
-                try {
-                    const [success, error] = await getAppUser(subtask?.user_id ?? "")
-                    if (success) {
-                        subtask.userName = success?.name ?? ""
-                        reportedSubtasks.value.push(subtask)
+export async function getreportedSubtasksList(firstCall: boolean) {
+	try {
+		const limit = useLimitReportedTasks();
+		const offset = useOffsetReportedTasks();
+		const noMoreSubtasks = useNoMoreSubtasks();
+
+		const response: ReportBase[] = await GET(
+			`/challenges/subtask_reports?limit=${limit.value}&offset=${offset.value}`
+		);
+
+		let arr: ReportBase[] = response ?? [];
+		if (!arr.length) {
+			noMoreSubtasks.value = true;
+			return openSnackbar("info", "Body.NoMoreReports");
+		}
+		if (arr.length < 10) {
+			noMoreSubtasks.value = true;
+		}
+		const reportedSubtasks = useReportedSubtasks();
+		console.log(reportedSubtasks.value, "reportetSubtasks");
+
+		if (firstCall) {
+			reportedSubtasks.value = [];
+		}
+
+		const allSubTasks = await GET(`challenges/subtasks`);
+
+		// Information: below code is used to append response
+		arr = [...reportedSubtasks.value, ...(response ?? [])];
+
+		arr.forEach((subTask: ReportBase) => {
+			subTask.creator_id = allSubTasks.find(
+				(allSubTask: any) => allSubTask.task_id === subTask.task_id
+			).creator;
+		});
+
+		arr.forEach(async (subtask: ReportBase, index: number) => {
+			if (index >= reportedSubtasks.value.length) {
+				try {
+					const [reporter, error] = await getAppUser(subtask?.user_id ?? "");
+					if (reporter) {
+						subtask.userName = reporter?.name ?? "";
+						reportedSubtasks.value.push(subtask);
+					}
+
+                    const [creator, creatorError] = await getAppUser(subtask.creator_id)
+                    if (creator){
+                        subtask.creatorName = creator.name ?? ""
                     }
-                } catch (error: any) {
-                    console.log("error in get app user", error)
-                }
-            }
-        });
-        // Todo: here are all subtasks. map subtasks.id === reportedSubtasks.subtask_id
-        const subTasks = await GET(`challenges/subtasks`)
-        console.log(subTasks, 'subTasks');
+                
+				} catch (error: any) {
+					console.log("error in get app user", error);
+				}
+			}
+		});
 
-        // reportedSubtasks.value = arr;
-
-        return [response, null];
-    } catch (error: any) {
-        return [null, error.data];
-    }
+		return [response, null];
+	} catch (error: any) {
+		return [null, error.data];
+	}
 }
 
 export async function resolveReport(report_id: any, body: any) {
-    try {
-        const res = await PUT(`/challenges/subtask_reports/${report_id}`, body)
-        return [res, null]
-    }
-    catch (error: any) {
-        let details = error?.data?.error ?? "";
-        if (details.includes("already_resolved")) {
-            error.data.detail = "Error.ReportAlreadyResolved";
-        } else if (details.includes("report_not_found")) {
-            error.data.detail = "Error.ReportNotFound";
-        }
+	try {
+		const res = await PUT(`/challenges/subtask_reports/${report_id}`, body);
+		return [res, null];
+	} catch (error: any) {
+		let details = error?.data?.error ?? "";
+		if (details.includes("already_resolved")) {
+			error.data.detail = "Error.ReportAlreadyResolved";
+		} else if (details.includes("report_not_found")) {
+			error.data.detail = "Error.ReportNotFound";
+		}
 
-        console.log("error is this", error.data)
-        return [null, error.data.detail]
-    }
+		console.log("error is this", error.data);
+		return [null, error.data.detail];
+	}
 }
 
 export async function getCodingChallenge(task_id: any, subtask_id: any) {
-    try {
-        const res = await GET(`/challenges/tasks/${task_id}/coding_challenges/${subtask_id}`)
-        await getCodingChallengeSolution(task_id, subtask_id)
-        const codingChallenge: any = useCodingChallenge()
-        codingChallenge.value = res ?? null
-        return [res, null]
-    }
-    catch (error: any) {
-        let details = error?.data?.error ?? "";
-        if (details.includes("subtask_not_found")) {
-            error.data.detail = "Error.CodingChallengeNotFound";
-        }
+	try {
+		const res = await GET(
+			`/challenges/tasks/${task_id}/coding_challenges/${subtask_id}`
+		);
+		await getCodingChallengeSolution(task_id, subtask_id);
+		const codingChallenge: any = useCodingChallenge();
+		codingChallenge.value = res ?? null;
+		return [res, null];
+	} catch (error: any) {
+		let details = error?.data?.error ?? "";
+		if (details.includes("subtask_not_found")) {
+			error.data.detail = "Error.CodingChallengeNotFound";
+		}
 
-        console.log("error is this", error.data)
-        return [null, error.data.detail]
-
-    }
+		console.log("error is this", error.data);
+		return [null, error.data.detail];
+	}
 }
 
-export async function getCodingChallengeSolution(task_id: any, subtask_id: any) {
-    try {
-        const res = await GET(`/challenges/tasks/${task_id}/coding_challenges/${subtask_id}/solution`)
-        const codingChallengeSolution: any = useCodingChallengeSolution()
-        codingChallengeSolution.value = res ?? null
-        return [res, null]
-    }
-    catch (error: any) {
-        return [null, error]
-
-    }
+export async function getCodingChallengeSolution(
+	task_id: any,
+	subtask_id: any
+) {
+	try {
+		const res = await GET(
+			`/challenges/tasks/${task_id}/coding_challenges/${subtask_id}/solution`
+		);
+		const codingChallengeSolution: any = useCodingChallengeSolution();
+		codingChallengeSolution.value = res ?? null;
+		return [res, null];
+	} catch (error: any) {
+		return [null, error];
+	}
 }
 
 export async function getMcq(task_id: any, subtask_id: any) {
-    try {
-        const res = await GET(`/challenges/tasks/${task_id}/multiple_choice/${subtask_id}/solution`)
-        const mcq: any = useMcq()
-        mcq.value = res ?? null
-        return [res, null]
-    }
-    catch (error: any) {
-        let details = error?.data?.error ?? "";
-        if (details.includes("subtask_not_found")) {
-            error.data.detail = "Error.McqNotFound";
-        }
+	try {
+		const res = await GET(
+			`/challenges/tasks/${task_id}/multiple_choice/${subtask_id}/solution`
+		);
+		const mcq: any = useMcq();
+		mcq.value = res ?? null;
+		return [res, null];
+	} catch (error: any) {
+		let details = error?.data?.error ?? "";
+		if (details.includes("subtask_not_found")) {
+			error.data.detail = "Error.McqNotFound";
+		}
 
-        console.log("error is this", error.data)
-        return [null, error.data.detail]
-
-    }
+		console.log("error is this", error.data);
+		return [null, error.data.detail];
+	}
 }
---
+++ types/reportedTaskTypes.ts
@@ -0,0 +1,17 @@
+
+
+export class ReportBase {
+    comment: string = ''
+    id: string = ''
+    reason: string = ''
+    subtask_id: string = ''
+    subtask_type: string = ''
+    task_id: string = ''
+    timestamp: string = ''
+    // Information: user_id & name is the reporter
+    user_id: string = ''
+    userName: string = ''
+    // Information: creator_id & name is the task-creator
+    creator_id: string = ''
+    creatorName: string = ''
+}
